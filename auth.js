8819bee705882eecae3f52fccf683e34357b5179
evelynnicolesilva-arch <evelyn.nicole.silva@escola.pr.gov.br>
2026-03-16 10:55:58 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,83 +2,35 @@ function cadastrar(){
 
 let usuario = document.getElementById("usuarioCadastro").value
 let senha = document.getElementById("senhaCadastro").value
+let msg = document.getElementById("msgCadastro")
 
-if(usuario === "" || senha === ""){
-alert("Preencha todos os campos")
+if(!usuario || !senha){
+
+msg.innerText = "Preencha todos os campos"
 return
+
 }
 
 let usuarios = JSON.parse(localStorage.getItem("usuarios")) || []
 
-let usuarioExistente = usuarios.find(u => u.usuario === usuario)
+let existe = usuarios.find(u => u.usuario === usuario)
 
-if(usuarioExistente){
-alert("Usuário já existe")
+if(existe){
+
+msg.innerText = "Usuário já existe"
 return
+
 }
 
 usuarios.push({
-usuario: usuario,
-senha: senha
+
+usuario:usuario,
+senha:senha
+
 })
 
-localStorage.setItem("usuarios", JSON.stringify(usuarios))
+localStorage.setItem("usuarios",JSON.stringify(usuarios))
 
-alert("Conta criada com sucesso!")
-
-window.location.href = "login.html"
-
-}
-
-
-
-function login(){
-
-let usuario = document.getElementById("usuarioLogin").value
-let senha = document.getElementById("senhaLogin").value
-
-let usuarios = JSON.parse(localStorage.getItem("usuarios")) || []
-
-let usuarioValido = usuarios.find(u => u.usuario === usuario && u.senha === senha)
-
-if(usuarioValido){
-
-localStorage.setItem("usuarioLogado", usuario)
-
-window.location.href = "perfil.html"
-
-}else{
-
-alert("Usuário ou senha incorretos")
-
-}
-
-}
-
-
-
-function mostrarUsuario(){
-
-let usuario = localStorage.getItem("usuarioLogado")
-
-if(usuario){
-
-document.getElementById("usuarioLogado").innerText = "Usuário logado: " + usuario
-
-}else{
-
-window.location.href = "login.html"
-
-}
-
-}
-
-
-
-function logout(){
-
-localStorage.removeItem("usuarioLogado")
-
-window.location.href = "login.html"
+msg.innerText = "Conta criada com sucesso!"
 
 }
